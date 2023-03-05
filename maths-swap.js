459ff124dab6cf52c8bff5fa1a67325d99a2add3
Pranab Kumar Das <98226247+prana6-kd@users.noreply.github.com>
2023-03-05 17:00:15 +0530

    Add files via upload

---
+++ maths-swap.js
@@ -0,0 +1,18 @@
+var a = 10, b=5;
+var sum = a+b;
+console.log(a);
+console.log(b);
+console.log(sum);
+
+a = sum -b;
+b= sum -a;
+console.log(a);
+console.log(b);
+
+
+for (let i =0 ; i<=10; i++){
+
+    let total = total + i;
+}
+
+console.log(total)
